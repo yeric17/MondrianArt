e906b48602b8f3abe5a26707ee2408879af991fc
yeric17 <yeric17@gmail.com>
2019-04-22 20:41:07 -0500

    commit inicial

---
+++ Assets/_Scripts/ARController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARController : DefaultTrackableEventHandler
+{
+
+     string thisName;
+
+    [SerializeField] GameObject objectToShow;
+
+
+    ImageTargetBehaviour thisImageBehaviour;
+
+
+    protected void Awake()
+    {
+        thisImageBehaviour = GetComponent<ImageTargetBehaviour>();
+
+    }
+    protected override void Start()
+    {
+        base.Start();
+        
+        
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        print("Encontrado el marcador: " + thisImageBehaviour.ImageTarget.Name);
+        if(objectToShow != null)
+        {
+
+            objectToShow.GetComponent<InteractiveARObject>().Interact();
+
+        }
+    }
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if (thisImageBehaviour.ImageTarget != null)
+        {
+            print("Perdido el marcadorasdasd: " + thisImageBehaviour.ImageTarget.Name);
+        }
+        if (objectToShow != null)
+            objectToShow.GetComponent<InteractiveARObject>().StopInteraction();
+
+    }
+}
---
+++ Assets/_Scripts/InteractiveARObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveARObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] AudioClip audioToPlay;
+    [SerializeField] string animName;
+
+    private AudioSource audioPlayer;
+
+    private Animator thisAnimator;
+    void Start()
+    {
+        if(GetComponentInChildren<AudioSource>())
+        audioPlayer = GetComponentInChildren<AudioSource>();
+        if(GetComponentInChildren<Animator>())
+        thisAnimator = GetComponentInChildren<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Interact()
+    {
+        print("OBJETO INTERACTUANDO");
+        if(audioToPlay != null)
+        {
+            audioPlayer.clip = audioToPlay;
+            audioPlayer.Play();
+        }
+        if(thisAnimator != null)
+        {
+            thisAnimator.SetTrigger(animName);
+        }
+    }
+
+    public void StopInteraction()
+    {
+        if(thisAnimator != null)
+        thisAnimator.SetTrigger("Idle");
+
+        if (audioPlayer != null)
+            audioPlayer.Stop();
+
+    }
+}
